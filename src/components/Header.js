4e1d9e790f00066aa25fa8655a7ff37c4689883d
Zsvob <svobodazachary@gmail.com>
2022-06-27 15:05:13 -0400

    header start

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+function Header() {
+    return (
+        <header className="flex-between">
+            <div>
+                <a>logo</a>
+                Omni Consumer Products</div>
+            <nav className="flex">
+                <div>About</div>
+                <div>Products</div>
+                <div>Register</div>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
