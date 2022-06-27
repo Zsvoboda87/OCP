9faa499b5b688a30451a8f5e41b24930cd853b57
Zsvob <svobodazachary@gmail.com>
2022-06-27 16:14:26 -0400

    carousel

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+import SlideDeck from './components/Carousel';
 import Header from './components/Header'
 
 function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <SlideDeck />
+    </>
   );
 }
 
